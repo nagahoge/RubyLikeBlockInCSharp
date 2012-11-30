46613936644d6c92c0f774db136a22a1e23ea9b3
nagahoge <v@nanaketa.com>
2012-12-01 08:10:35 +0900

    Add IO.Foreach method.

---
+++ RubyLikeBlockInCSharp/IO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RubyLikeBlock
+{
+    public class IO
+    {
+        public static void Foreach(string path, Encoding encoding, Action<string> block)
+        {
+            using (var reader = new StreamReader(path, encoding))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    block(line);
+                }
+            }
+        }
+
+        public static string[] Foreach(string path, Encoding encoding)
+        {
+            var lines = new List<string>();
+            IO.Foreach(path, encoding, line => lines.Add(line));
+            return lines.ToArray();
+        }
+
+        public static void Foreach(string path, Action<string> block)
+        {
+            IO.Foreach(path, Encoding.Default, block);
+        }
+
+        public static void Foreach(string path)
+        {
+            IO.Foreach(path, Encoding.Default);
+        }
+    }
+}
---
+++ UnitTest/IOTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+using RubyLikeBlock;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class IOTest
+    {
+        [TestMethod]
+        public void TestForeach()
+        {
+            const string path = "FileExtensionTest_TestForeachData.txt";
+            string[] lines = _.Ary("First line.", "Second line.", "Third line.");
+
+            using (var writer = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                lines.Each(str => writer.WriteLine(str));
+            }
+
+            CollectionAssert.AreEqual(lines, IO.Foreach(path, Encoding.UTF8));
+
+
+            lines = new string[] { };
+            using (var writer = new StreamWriter(path, false, Encoding.UTF8)){}
+            CollectionAssert.AreEqual(lines, IO.Foreach(path, Encoding.UTF8));
+        }
+    }
+}
